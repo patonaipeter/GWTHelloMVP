b00e65c3e10831d751ecbf7e0590dcf5f19d311a
Patonai <patonaipeter@gmail.com>
2013-06-07 23:44:07 +0200

    initial

---
+++ GWTHelloMVP/src/main/java/com/hellomvp/client/place/HelloPlace.java
@@ -0,0 +1,47 @@
+package com.hellomvp.client.place;
+
+import com.google.gwt.place.shared.Place;
+import com.google.gwt.place.shared.PlaceTokenizer;
+
+//public class HelloPlace extends ActivityPlace<HelloActivity>
+public class HelloPlace extends Place
+{
+	private String helloName;
+	
+	public HelloPlace(String token)
+	{
+		this.helloName = token;
+	}
+
+	public String getHelloName()
+	{
+		return helloName;
+	}
+
+	public static class Tokenizer implements PlaceTokenizer<HelloPlace>
+	{
+
+		public String getToken(HelloPlace place)
+		{
+			return place.getHelloName();
+		}
+
+		public HelloPlace getPlace(String token)
+		{
+			return new HelloPlace(token);
+		}
+
+	}
+	
+//	@Override
+//	protected Place getPlace(String token)
+//	{
+//		return new HelloPlace(token);
+//	}
+//
+//	@Override
+//	protected Activity getActivity()
+//	{
+//		return new HelloActivity("David");
+//	}
+}
